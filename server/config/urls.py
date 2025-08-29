eff15afd468bbcbbca9486d019a941732dea9491
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-08-29 19:34:19 +0530

    Customized Token Payload

--- server/config/urls.py
+++ server/config/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from user.views import MyTokenObtainPairView 
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/', MyTokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/', include('books.urls')),
     path('api/', include('library.urls')),
