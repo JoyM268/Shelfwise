25e1a59355544d51aaebffe948f587f647803116
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-08-30 20:56:43 +0530

    Updated deployement configurations

--- server/config/settings.py
+++ server/config/settings.py
@@ -62,6 +62,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+     'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
