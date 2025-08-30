b989b4f9e03565bc573c7f6487c422efa239e18e
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-08-30 18:31:23 +0530

    Minor Changes

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import dayjs from "dayjs";
 
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = import.meta.env.VITE_SERVER_URL || "http://127.0.0.1:8000";
 
 let authTokens = localStorage.getItem("authTokens")
 	? JSON.parse(localStorage.getItem("authTokens") as string)
