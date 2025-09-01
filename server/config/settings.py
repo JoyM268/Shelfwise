4a1e9d71d89ddcc3bbdf4a694ddc473dc2e4636c
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-09-01 22:06:59 +0530

    Minor config change

--- server/config/settings.py
+++ server/config/settings.py
@@ -18,7 +18,6 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv() 
 GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 
 # Quick-start development settings - unsuitable for production
